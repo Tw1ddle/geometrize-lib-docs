16a7483dcb0acf6288fbf31f017857676520a76d
sam <sam@silver42.karoo.co.uk>
2017-06-20 22:22:03 +0100

    no message

--- travis_update_geometrize_submodule.sh
+++ travis_update_geometrize_submodule.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script gets the latest changes from the submodule(s), commits the changes, and pushes to the remote (only if there are any changes, to avoid an infinite build loop)
+
 git submodule update --remote --merge
 
 if [[ `git status --porcelain` ]]; then
@@ -8,10 +10,8 @@ if [[ `git status --porcelain` ]]; then
   git config --global user.name "Travis CI Bot"
   git checkout master
   git add geometrize
-  git commit --allow-empty --message "Travis bot updating submodule and rebuilding documentation"
+  git commit --message "Travis bot updating submodule and rebuilding documentation"
   git remote rm origin
   git remote add origin https://${GITHUB_API_KEY}@github.com/Tw1ddle/geometrize-lib-docs.git > /dev/null 2>&1
   git push origin master
-else
-  # No pushing necessary
 fi
