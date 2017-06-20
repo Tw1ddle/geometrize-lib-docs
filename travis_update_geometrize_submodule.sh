83705611124095af3584ddec3c06468cde766bad
sam <sam@silver42.karoo.co.uk>
2017-06-20 22:08:31 +0100

    More build script fumbling, we can't do empty commits because then travis will build endlessly

---
+++ travis_update_geometrize_submodule.sh
@@ -0,0 +1,17 @@
+#!bin/sh
+
+git submodule update --remote --merge
+
+if [[ `git status --porcelain` ]]; then
+  # Push the latest version of the Geometrize submodule to the repository
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI Bot"
+  git checkout master
+  git add geometrize
+  git commit --allow-empty --message "Travis bot updating submodule and rebuilding documentation"
+  git remote rm origin
+  git remote add origin https://${GITHUB_API_KEY}@github.com/Tw1ddle/geometrize-lib-docs.git > /dev/null 2>&1
+  git push origin master
+else
+  # No pushing necessary
+fi
